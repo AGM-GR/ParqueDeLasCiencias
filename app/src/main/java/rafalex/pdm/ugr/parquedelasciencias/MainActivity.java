dca33c7c0e48311f6b07ec2c203dacb6066c39a7
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2017-04-26 17:45:56 +0200

    Revert "Multiidioma, estilo barra menu y dialogo ayuda."
    This reverts commit f61074b20967147704b2444999e10ef1963cc7b7.

--- app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/MainActivity.java
+++ app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/MainActivity.java
@@ -1,57 +1,13 @@
 package rafalex.pdm.ugr.parquedelasciencias;
 
-import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    //Datos para dialogos
-    private AlertDialog menuDialog;
-    private AlertDialog.Builder helpDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Crea los dialogos
-        LayoutInflater inflater = this.getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.help_dialog, null);
-
-        helpDialog = new AlertDialog.Builder(this, R.style.DialogTheme)
-                .setView(dialogView)
-                .setTitle(R.string.menu_help)
-                .setNeutralButton(R.string.ok_button,null);
-
-        menuDialog = helpDialog.create();
-    }
-
-
-    //Función onCreateOptionMenu, para añadir el estilo de nuestro action_bar
-    @Override
-    public boolean onCreateOptionsMenu (Menu menu) {
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.action_bar, menu); // set your file name
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    //Función onOptionItemSelected, para definir el funcionamiento de las opciones del action_bar
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-
-        if (item.getItemId() == R.id.help) {
-
-            menuDialog.show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
