09d5dc5ae6af4b2e47462b59e5b97ee801a318b3
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2017-05-05 13:39:23 +0200

    Intenta leer los codigos QR.

--- app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/MainActivity.java
+++ app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/MainActivity.java
@@ -1,6 +1,10 @@
 package rafalex.pdm.ugr.parquedelasciencias;
 
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,8 +12,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 123;
+
+    Button scannButton;
 
     //Datos para dialogos
     private AlertDialog menuDialog;
@@ -19,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scannButton = (Button) findViewById(R.id.scannButton);
+
+        scannButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, QRScanner.class);
+                startActivity(i);
+            }
+        });
+
+        //Pide permisos para la camara
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT > 22) {
+                if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA))
+                    Toast.makeText(getApplicationContext(), "Se necesita acceso a la camara", Toast.LENGTH_SHORT).show();
+                requestPermissions(new String[]{android.Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+        }
 
         //Crea los dialogos
         LayoutInflater inflater = this.getLayoutInflater();
---
+++ app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/QRScanner.java
@@ -0,0 +1,102 @@
+package rafalex.pdm.ugr.parquedelasciencias;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class QRScanner extends AppCompatActivity {
+
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 123;
+
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    SurfaceView cameraView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscanner);
+
+        cameraView = (SurfaceView) findViewById(R.id.camera_view);
+
+        //Creamos el lector de qr
+        barcodeDetector =
+                new BarcodeDetector.Builder(getBaseContext())
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        //Creamos la camara
+        cameraSource = new CameraSource
+                .Builder(getBaseContext(), barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        // listener de ciclo de vida de la camara
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                //Verificamos si el usuario ha dado permiso para la camara, en caso contrario volvemos a pedirlos
+                if (ContextCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException ie) {
+                        Log.e("CAMERA SOURCE", ie.getMessage());
+                    }
+                } else {
+                    if (Build.VERSION.SDK_INT > 22) {
+                        if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA))
+                            Toast.makeText(getApplicationContext(), "Se necesita acceso a la camara", Toast.LENGTH_SHORT).show();
+                        requestPermissions(new String[]{android.Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        // preparo el detector de QR
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    String value = barcodes.valueAt(0).displayValue.toString();
+                    Toast.makeText(getApplicationContext(), value, Toast.LENGTH_SHORT).show();
+                }
+
+                barcodeDetector.release();
+            }
+        });
+
+
+    }
+
+}
