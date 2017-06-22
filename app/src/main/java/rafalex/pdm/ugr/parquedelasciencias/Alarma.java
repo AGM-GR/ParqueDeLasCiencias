f8ed65820263557bdf0e1d3722fb47f3f4f8801b
mrmedfac9 <ramefa@hotmail.com>
2017-06-22 14:23:06 +0200

    Vibracion

--- app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/Alarma.java
+++ app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/Alarma.java
@@ -1,11 +1,16 @@
 package rafalex.pdm.ugr.parquedelasciencias;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Alarma extends AppCompatActivity {
+
+    Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+    long [] patron = {0, 500, 500, 500, 500};
 
     TextView titulo;
     TextView hora;
@@ -19,5 +24,19 @@ public class Alarma extends AppCompatActivity {
         titulo = (TextView) findViewById(R.id.titulo_alarma);
         hora = (TextView) findViewById(R.id.hora_alarma);
         aceptar = (Button) findViewById(R.id.aceptar_alarma);
+
+        aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vib.cancel();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        vib.vibrate(patron,-1);
     }
 }
--- app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/Temporizador.java
+++ app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/Temporizador.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 
 public class Temporizador extends BroadcastReceiver {
 
+    String titulo;
+    String hora;
+
     @Override
     public void onReceive(Context context, Intent intent)  {
         Intent actividadALanzar = new Intent(context, Alarma.class);
+
+        actividadALanzar.putExtra("titulo",titulo);
+        actividadALanzar.putExtra("hora",hora);
+
         context.startActivity(actividadALanzar);
     }
 }
