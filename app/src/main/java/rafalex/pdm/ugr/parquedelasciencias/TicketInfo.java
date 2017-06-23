b889c4e95dd7b9a63509f32fbb6bc4009be6273c
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2017-06-23 16:57:06 +0200

    Volver a escanear.

--- app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/TicketInfo.java
+++ app/src/main/java/rafalex/pdm/ugr/parquedelasciencias/TicketInfo.java
@@ -97,7 +97,15 @@ public class TicketInfo extends AppCompatActivity
         } else if (id == R.id.mapa) {
             fragmentManager.beginTransaction().replace(R.id.contenedor, new MapaFragment()).commit();
         } else if (id == R.id.escanear) {
-            fragmentManager.beginTransaction().replace(R.id.contenedor, new MapaFragment()).commit();
+            //Borra la entrada guardada
+            SharedPreferences.Editor entrada_escaneada = getSharedPreferences("Entrada", Context.MODE_PRIVATE).edit();
+            entrada_escaneada.putString("codigo", "");
+            entrada_escaneada.commit();
+
+            //Lanza el escaneo
+            Intent i = new Intent(TicketInfo.this, QRScanner.class);
+            startActivity(i);
+
         } else if (id == R.id.ayuda) {
             fragmentManager.beginTransaction().replace(R.id.contenedor, new MapaFragment()).commit();
         }
